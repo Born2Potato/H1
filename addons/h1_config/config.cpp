0840a4fd83e281daaf2925ba725536e94fd49b61
Steve Zhao <ampersand38@gmail.com>
2025-07-15 14:26:01 -0400

    Use A3_Data_F_Decade_Loadorder in requiredAddons instead of multiple A3 addons

--- addons/h1_config/config.cpp
+++ addons/h1_config/config.cpp
@@ -5,7 +5,7 @@ class CfgPatches
 		units[] = {"uh1y"};
 		weapons[] = {};
 		requiredVersion = 1.0;
-		requiredAddons[] = {"A3_Air_F", "A3_Weapons_F", "A3_Soft_F", "A3_data_F", "A3_Characters_F"};
+		requiredAddons[] = {"h1_uh1y", "h1_hydra", "h1_ui", "h1_sounds", "h1_misc"};
 	};
 };
 #include "config\misc.hpp"
--- addons/h1_sounds/config.cpp
+++ addons/h1_sounds/config.cpp
@@ -5,6 +5,6 @@ class CfgPatches
 		units[] = {"uh1y"};
 		weapons[] = {};
 		requiredVersion = 1.0;
-		requiredAddons[] = {"A3_Air_F", "A3_Weapons_F", "A3_Soft_F", "A3_data_F", "A3_Characters_F"};
+		requiredAddons[] = {"A3_Data_F_Decade_Loadorder"};
 	};
 };
